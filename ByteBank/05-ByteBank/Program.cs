fd09efbaaa3ec534e2f046aef4bf7856108b1783
Andrey Silva <andreey.siilva@live.com>
2022-01-06 01:00:07 -0300

    Classe Cliente
    Feito até a classe do cliente.
    Informações do Cliente.

---
+++ ByteBank/05-ByteBank/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente gabriela = new Cliente();
+
+            gabriela.nome = "Gabriela";
+            gabriela.profissao = "Desenvolvedora C#";
+            gabriela.cpf = "434.562.878-20";
+        }
+    }
+}
